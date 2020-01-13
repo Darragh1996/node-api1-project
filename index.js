0c08baef2cda67dd2173ced961ff09cce78b29aa
Darragh Ferry <darraghferry@hotmail.com>
2020-01-13 13:21:26 +0000

    basic framework for server in place

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // implement your API here
+const express = require("express");
+
+const { find, findById, insert, update, remove } = require("./data/db");
+
+const app = express();
+
+app.use(express.json());
+
+app.post("/api/users", (req, res) => {
+  console.log(req.body);
+  const { name, bio } = req.body;
+  if (!name || !bio) {
+    res
+      .status(400)
+      .json({ message: "Please provide name and bio for the user" });
+  } else {
+    insert();
+  }
+});
+
+app.listen(3000, () => {
+  console.log("listening on port 3000");
+});
